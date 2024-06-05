e5f727a8be5d9fbac94bcfe212ad15f68bb566be
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2024-06-05 17:16:41 -0300

    a
    a

--- UI/ui/src/ui/acoes.clj
+++ UI/ui/src/ui/acoes.clj
@@ -1,17 +1,25 @@
 ;; NAMESPACE
 (ns ui.acoes
-    (:require 
+    (:require
+		;;dependencias
 		[clojure.tools.cli :refer [parse-opts]]
-        [cheshire.core :refer [parse-string]]
+        [cheshire.core :refer [parse-string generate-string]]
         [clj-http.client :as http]
 
+		;;nativas
+        [clojure.string :refer [split]]
+
+		;;aux
 		[ui.auxiliar :refer :all]
         )
 )
 ;;
 
-;;infos (TODO: REFATORAR)
+;;TODO: REFATORAR
 (def portaPadrao 3000)
+(defn prepMostrarTransacao [transacao]
+	(println (str "Valor: " (subs (nth transacao 0) 8) "\nTipo: " (subs (nth transacao 1) 8 (dec (count (nth transacao 1))))))
+)
 
 ;;ACOES
 (defn cadastrarTransacao []
@@ -37,16 +45,19 @@
 	(limparTerminal) ;limpa terminal
 	(println "(DEBUG) exibir transacoes")
 
-	(let [transacoes (parse-string (parse-string (:body (http/get (str "http://localhost:" portaPadrao "/transacao")
-			{:query-params {:valor :tipo} :content-type :json}))))]
-		(println (str "quantidade: " (count transacoes) "\n\n"))
-		(println transacoes)
-		;(println (count (nth transacoes 0)))
+	(let [transacoes (:body (http/get (str "http://localhost:" portaPadrao "/transacao")
+			{:query-params {:valor :tipo} :content-type :json}))]
+		(let [transacoes (parse-string(subs (str transacoes) 13 (dec (count transacoes))))]
+			;(println transacoes)
+			;(println (nth transacoes 0))
+			(let [transacoes (map (fn[S](split (subs (str S) 1 (dec (count (str S)))) #", ")) transacoes)]
+				;(println transacoes)
+				;(println (nth (nth transacoes 0) 1))
+				;(prepMostrarTransacao (nth transacoes 0))
+				(doall (map #(prepMostrarTransacao (nth transacoes #)) (range 0 (count transacoes))))
+			)
+		)
 	)
-
-    ;; (print (:body (http/get (str "localhost:" portaPadrao "/transacao")
-    ;; {:query-params {"valor" valor "apiKey" "CHAVE!!" }}
-	;; )))
 	)
 ;;
 
--- UI/ui/src/ui/core.clj
+++ UI/ui/src/ui/core.clj
@@ -3,6 +3,7 @@
 	(:gen-class)
 	(:require
 		[ui.interfaceamento :refer :all]
+		[ui.acoes :refer [exibirTransacoes]]
 	))
 ;;
 
@@ -10,7 +11,9 @@
 (defn -main
 	"I don't do a whole lot ... yet."
 	[& args]
+	(exibirTransacoes)
+
 	;mostra opcoes de ação e pede escolha do usuario
-	(escolherAcao)
+	;(escolherAcao)
 	)
 ;;
