a4e561ee9682b92e8acca7a51e2e82ee4016365e
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2024-06-05 17:40:55 -0300

    Update acoes.clj

--- UI/ui/src/ui/acoes.clj
+++ UI/ui/src/ui/acoes.clj
@@ -18,7 +18,7 @@
 ;;TODO: REFATORAR
 (def portaPadrao 3000)
 (defn prepMostrarTransacao [transacao]
-	(println (str "Valor: " (subs (nth transacao 0) 8) "\nTipo: " (subs (nth transacao 1) 8 (dec (count (nth transacao 1))))))
+	(println (str "\nValor: " (subs (nth transacao 0) 8) "\nTipo: " (subs (nth transacao 1) 8 (dec (count (nth transacao 1))))))
 )
 
 ;;ACOES
@@ -48,13 +48,13 @@
 	(let [transacoes (:body (http/get (str "http://localhost:" portaPadrao "/transacao")
 			{:query-params {:valor :tipo} :content-type :json}))]
 		(let [transacoes (parse-string(subs (str transacoes) 13 (dec (count transacoes))))]
-			;(println transacoes)
-			;(println (nth transacoes 0))
 			(let [transacoes (map (fn[S](split (subs (str S) 1 (dec (count (str S)))) #", ")) transacoes)]
-				;(println transacoes)
 				;(println (nth (nth transacoes 0) 1))
-				;(prepMostrarTransacao (nth transacoes 0))
-				(doall (map #(prepMostrarTransacao (nth transacoes #)) (range 0 (count transacoes))))
+				(prepMostrarTransacao (nth transacoes 0))
+				(doall (map #(do 
+					(println (str "TRANSACAO " %))
+					(prepMostrarTransacao (nth transacoes %))
+				) (range 0 (count transacoes))))
 			)
 		)
 	)
