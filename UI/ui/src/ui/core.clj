e3be9fb098f15cce437c9edc340f85f1b7cb4cb8
C-dS-R <carolinadesouzaribeiro@hotmail.com>
2024-06-09 19:17:44 -0300

    Update core.clj

--- UI/ui/src/ui/core.clj
+++ UI/ui/src/ui/core.clj
@@ -3,7 +3,6 @@
 	(:gen-class)
 	(:require
 		[ui.interfaceamento :refer :all]
-		;[ui.acoes :refer [exibirTransacoes]]
 	))
 ;;
 
@@ -11,8 +10,6 @@
 (defn -main
 	"I don't do a whole lot ... yet."
 	[& args]
-	;(exibirTransacoes)
-
 	;mostra opcoes de ação e pede escolha do usuario
 	(escolherAcao)
 	)
